1e3a164ba948ea1e95c48a418a28c2879e9f9932
Hoksolinvan Chhun <158796823+Hoksolinvan@users.noreply.github.com>
2026-04-30 11:16:00 -0700

    Add splitter function to helper.h for string parsing

---
+++ src/helper.h
@@ -0,0 +1,111 @@
+#pragma once
+#include <vector>
+#include <sstream>
+#include <string>
+
+namespace helper{
+inline std::vector<std::string> splitter(std::string input,char delimiter=' '){
+
+    // std::vector<std::string> result;
+    // std::string word;
+    // bool quote_on = false;
+    // std::stringstream ss(input);
+
+    // while(std::getline(ss,word,delimiter)){
+
+    //     if(word=="\'"){
+    //         quote_on = !quote_on;
+    //         continue;
+    //     }
+
+
+    //     if(word==" " && !quote_on){
+    //         continue;
+    //     }
+
+    //     result.push_back(word);
+    // }
+   
+    // return result;
+
+
+
+    // std::vector<std::string> result;
+    // std::string word;
+    // bool quote_on = false;
+    // std::string previous ="";
+    
+    
+    // for(char c : input){
+        
+    //     if(c=='\''){
+    //         quote_on = !quote_on;
+    //         continue;
+    //     }
+        
+        
+    //     if(c==' '){
+    //         if(!word.empty()){
+    //             result.push_back(word.c_str());
+    //             word.clear();
+    //             result.push_back(" ");
+    //         }
+    //         else if(quote_on){
+    //             result.push_back(" ");
+    //         }
+            
+    //     }
+    //     else{
+           
+    //         word+=c;
+    //     }
+    // }
+    
+    // if(!word.empty()){
+    // result.push_back(word);
+    // }
+    
+    
+    // return result;
+
+    std::vector<std::string> result;
+    std::string word;
+    bool quote_on = false;
+    std::string previous ="";
+    
+    
+    for(char c : input){
+        
+        if(c=='\''){
+            quote_on = !quote_on;
+            continue;
+        }
+        
+        
+        if(c==delimiter){
+            if(quote_on){
+                word+=" ";
+            }
+            else if(!word.empty()){
+                result.push_back(word);
+                word.clear();
+                result.push_back(" ");
+            }
+            
+            
+        }
+        else{
+            
+            word+=c;
+        }
+    }
+    
+    
+    if(!word.empty()){
+    result.push_back(word);
+    }
+    
+    
+    return result;
+}
+}
