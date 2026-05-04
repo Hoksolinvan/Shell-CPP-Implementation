1b1463de802f8a58bc1d024f16750ca394b0d9b3
Hoksolinvan Chhun <158796823+Hoksolinvan@users.noreply.github.com>
2026-05-04 09:39:16 -0700

    Update helper.h

--- src/helper.h
+++ src/helper.h
@@ -12,67 +12,6 @@
 namespace helper{
 inline std::vector<std::string> splitter(std::string input,char delimiter=' '){
 
-    // std::vector<std::string> result;
-    // std::string word;
-    // bool quote_on = false;
-    // std::stringstream ss(input);
-
-    // while(std::getline(ss,word,delimiter)){
-
-    //     if(word=="\'"){
-    //         quote_on = !quote_on;
-    //         continue;
-    //     }
-
-
-    //     if(word==" " && !quote_on){
-    //         continue;
-    //     }
-
-    //     result.push_back(word);
-    // }
-   
-    // return result;
-
-
-
-    // std::vector<std::string> result;
-    // std::string word;
-    // bool quote_on = false;
-    // std::string previous ="";
-    
-    
-    // for(char c : input){
-        
-    //     if(c=='\''){
-    //         quote_on = !quote_on;
-    //         continue;
-    //     }
-        
-        
-    //     if(c==' '){
-    //         if(!word.empty()){
-    //             result.push_back(word.c_str());
-    //             word.clear();
-    //             result.push_back(" ");
-    //         }
-    //         else if(quote_on){
-    //             result.push_back(" ");
-    //         }
-            
-    //     }
-    //     else{
-           
-    //         word+=c;
-    //     }
-    // }
-    
-    // if(!word.empty()){
-    // result.push_back(word);
-    // }
-    
-    
-    // return result;
 
     std::vector<std::string> result;
     std::string word;
